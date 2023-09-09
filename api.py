0ad751fb1d5928e0bfae4e5e23e7964c982fa4e6
rozas <enricofermi2019@gmail.com>
2023-09-09 11:08:14 -0300

    api com a tabela do usuario

---
+++ api.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY']='THISISSECRET'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////mnt/c/User/vicrrs/api_example/todo.db'
+
+db = SQLAlchemy(app)
+
+# Tabela do usuario
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.Boolean)
+    admin = db.Column(db.Boolean)
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
